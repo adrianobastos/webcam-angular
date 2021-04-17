aee9c1e2b12d43382e032a7f6f7b09811672cab2
Adriano Bastos <adriano.bastos@araisan.io>
2021-04-17 10:52:48 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WebcamModule } from 'ngx-webcam';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    WebcamModule,
     AppRoutingModule
   ],
   providers: [],
